df9a0b1fb28ff340a22915a6dd4cbce8d09ff922
Arjan de Ruijter <rra@mboutrecht.nl>
2013-11-28 14:03:14 +0100

    Logboek gewijzigd en de StartScene class opgebouwd

--- PyramidPanic-am1b/PyramidPanic-am1b/GameScenes/StartScene/StartScene.cs
+++ PyramidPanic-am1b/PyramidPanic-am1b/GameScenes/StartScene/StartScene.cs
@@ -1,11 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 
-namespace PyramidPanic.GameScenes.StartScene
+
+namespace PyramidPanic
 {
-    class StartScene
+    public class StartScene
     {
+        //Fields
+        private PyramidPanic game;
+
+        //Constructor
+        public StartScene(PyramidPanic game)
+        {
+            this.game = game;
+            this.Initialize();
+        }
+
+        //Initialize
+        public void Initialize()
+        {
+            this.LoadContent();
+        }
+
+
+        //LoadContent
+        public void LoadContent()
+        {
+
+        }
+
+
+        //Update
+        public void Update(GameTime gameTime)
+        {
+
+        }
+        
+        //Draw
+        public void Draw(GameTime gameTime)
+        {
+
+        }
     }
 }
