0e08ef7b0320e4331ef9b715e52532637bbb2157
Arjan de Ruijter <rra@mboutrecht.nl>
2014-01-17 09:28:32 +0100

    Beetle en IBeetleState class respectievelijk interface gemaakt

---
+++ PyramidPanic-am1b/PyramidPanic-am1b/GameScenes/PlayScene/Beetle/IBeetleState.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PyramidPanic.GameScenes.PlayScene.Beetle
+{
+    interface IBeetleState
+    {
+    }
+}
